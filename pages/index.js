b2763e4a372f69d7d7442625352e78b4d0dc3ceb
Ernesto Mejias Alvarez <ernepazzo1212@email.com>
2022-02-10 14:57:59 -0500

    work - uci

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
+import { experiences, skills } from "../profile";
 
 const Index = () => (
   <>
@@ -37,6 +39,22 @@ const Index = () => (
           <div className="card bg-ligth">
             <div className="card-body">
               <h1>Skills</h1>
+
+              {skills.map(({ skill, percentage }, i) => (
+                <div className="py-3" key={i}>
+                  <h5>{skill}</h5>
+                  <div className="progress">
+                    <div
+                      className="progress-bar"
+                      role="progressbar"
+                      style={{ width: `${percentage}%` }}
+                      aria-valuenow="50"
+                      aria-valuemin="0"
+                      aria-valuemax="100"
+                    ></div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -44,6 +62,46 @@ const Index = () => (
           <div className="card bg-ligth">
             <div className="card-body">
               <h1>Experience</h1>
+
+              <ul>
+                {experiences.map(({ title, description, from, to }, index) => (
+                  <li key={index}>
+                    <h3>{title}</h3>
+                    <h5>
+                      {from}-{to}
+                    </h5>
+                    <p>{description}</p>
+                  </li>
+                ))}
+              </ul>
+
+              <Link href="/experiences">
+                <a className="btn btn-ligth">Know More</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Portfolio */}
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-dark">
+            <div className="row">
+              <div className="col-md-12">
+                <h1 className="text-center text-light">Portfolio</h1>
+              </div>
+
+              <div className="col-md-4">
+                <div className="card">
+                  <img src="portfolio1.jpeg" alt="" />
+                  <div className="card-body">
+                    <h3>titler</h3>
+                    <p>descriptions</p>
+                    <a href="#!">Know more</a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
---
+++ profile.js
@@ -0,0 +1,61 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 100,
+  },
+  {
+    skill: "Golang",
+    percentage: 80,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 90,
+  },
+  {
+    skill: "Blockchain",
+    percentage: 40,
+  },
+  {
+    skill: "Physics",
+    percentage: 70,
+  },
+  {
+    skill: "Enconomics",
+    percentage: 70,
+  },
+  {
+    skill: "Machine learning",
+    percentage: 70,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software developer at Microsoft",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices. Pretium fusce id velit ut tortor pretium viverra. Amet commodo nulla.",
+    from: 2000,
+    to: 2005,
+  },
+  {
+    title: "Software developer at Google",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices.",
+    from: 2005,
+    to: 2007,
+  },
+  {
+    title: "Software developer at SpaceX",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris a.",
+    from: 2010,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome website 1",
+    description: "Pretium fusce id velit ut tortor pretium viverra. Amet commodo nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "portfolio 1.jpeg"
+  },
+];
